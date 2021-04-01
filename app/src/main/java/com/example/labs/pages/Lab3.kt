f3756694f73a0a0ed11fb878d19077b808703169
Lagutov Alexander <lagutov.99@mail.ru>
2021-04-01 04:53:51 +0300

    lab3

--- app/src/main/java/com/example/labs/pages/Lab3.kt
+++ app/src/main/java/com/example/labs/pages/Lab3.kt
@@ -1,30 +1,20 @@
 package com.example.labs.pages
 
-import android.Manifest
-import android.content.pm.PackageManager
-import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.Toast
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.ImageCapture
-import androidx.camera.core.ImageCaptureException
-import androidx.camera.core.Preview
-import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
+import android.widget.TextView
+import androidx.core.text.htmlEncode
+import androidx.core.text.parseAsHtml
 import androidx.fragment.app.Fragment
 import com.example.labs.R
 import kotlinx.android.synthetic.main.lab3_frag.*
-import java.io.File
-import java.text.SimpleDateFormat
+import kotlinx.coroutines.*
 import java.util.*
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
+import java.net.URL
+import org.jsoup.Jsoup
 
 typealias LumaListener = (luma: Double) -> Unit
 
@@ -36,9 +26,32 @@ class Lab3 : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.lab3_frag, container, false)
+        val scope = CoroutineScope(Job())
+        val number : TextView = root.findViewById(R.id.number)
+        val responseView : TextView = root.findViewById(R.id.response)
+        val downloadBtn : Button = root.findViewById(R.id.download_button)
+        var url = "https://yandex.ru/pogoda/moscow?utm_source=serp&utm_campaign=wizard&utm_medium=desktop&utm_content=wizard_desktop_main&utm_term=title"
+        var response : String
+        downloadBtn.setOnClickListener{
+            val job = scope.async {
+                fetchWebsiteContents(url)
+            }
+            MainScope().launch {
+                responseView.text = job.await()
+                val text = responseView.text
+                val key ="Текущая температура"
+                val start = text.indexOf(key) + key.length
+                val end = text.indexOf(" ", start)
+                number.text = text.substring(start,end)
+            }
 
+        }
 
         return root
+    }
+    suspend fun fetchWebsiteContents(url: String):String {
+
+        return Jsoup.connect(url).get().text()
     }
 
 
